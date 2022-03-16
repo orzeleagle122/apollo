93023ed7ffaa1110fb363a11e1a026f4d28bbd8c
Patryk Orłowski <orzeleagle122@gmail.com>
2022-03-16 12:39:55 +0100

    install dep, add wslink and httplink in client.ts

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,7 +1,30 @@
-import {ApolloClient, InMemoryCache} from "@apollo/client";
+import {ApolloClient, InMemoryCache,split, HttpLink} from "@apollo/client";
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+import {getMainDefinition} from "@apollo/client/utilities";
+
+const wsLink = new GraphQLWsLink(createClient({
+    url: 'ws://graphql-compose.herokuapp.com/northwind/',
+}));
+
+const httpLink = new HttpLink({
+    uri: 'https://graphql-compose.herokuapp.com/northwind/'
+});
+
+const splitLink = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+            definition.kind === 'OperationDefinition' &&
+            definition.operation === 'subscription'
+        );
+    },
+    wsLink,
+    httpLink,
+);
 
 export const client = new ApolloClient({
-    uri: 'https://graphql-compose.herokuapp.com/northwind/',
+    link: splitLink,
     cache: new InMemoryCache({
         typePolicies: {
             Query:{

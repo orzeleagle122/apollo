ee4c51e4545e0d94baf7c594f91f88e9398f5f00
Patryk Orłowski <orzeleagle122@gmail.com>
2022-03-20 11:12:09 +0100

    fix

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -2,13 +2,26 @@ import {ApolloClient, InMemoryCache,split, HttpLink} from "@apollo/client";
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { createClient } from 'graphql-ws';
 import {getMainDefinition} from "@apollo/client/utilities";
+import {WebSocketLink} from "@apollo/client/link/ws";
 
-const wsLink = new GraphQLWsLink(createClient({
-    url: 'wss://graphql-compose.herokuapp.com/northwind/',
-}));
+// const wsLink = new GraphQLWsLink(createClient({
+//     url: 'wss://graphql-compose.herokuapp.com/northwind/',
+// }));
+
+const wsLink = new WebSocketLink({
+    uri: 'wss://graphql-compose.herokuapp.com/northwind/',
+    options: {
+        reconnect:true,
+    },
+})
 
 const httpLink = new HttpLink({
-    uri: 'https://graphql-compose.herokuapp.com/northwind/'
+    uri: 'https://graphql-compose.herokuapp.com/northwind/',
+    headers: {
+        'x-api-key': 'da2-g462acl3rnclplgtyxmbcqircy',
+        'x-rapidapi-host': 'schedule-import.p.rapidapi.com',
+        'x-rapidapi-key': 'c0b70ff419mshf6abc2a144dfa76p19dbe1jsn4a93647205b3'
+    }
 });
 
 const splitLink = split(
